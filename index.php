566c0bef712ac77edd7113398aa9a8d0380d014b
dawid2k20 <dawid.ordon2000@gmail.com>
2023-04-22 19:00:24 +0200

    v1.0

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
     session_start();
     include_once("./scripts/connect.php");
+    head();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 <body>
     <a href="./pattern_generator.php">Pattern generator</a><br />
     <a href="./password_pattern.php">Zmiana hasła na patterny</a><br />
