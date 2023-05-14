7a6654d3bba039e8870bca1b215724738c5a1198
dawid2k20 <dawid.ordon2000@gmail.com>
2023-05-14 19:14:58 +0200

    v1.5.2

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,7 +1,10 @@
 <?php
     function connect()
     {
-        $db = new mysqli("fakebankdawidordon.mysql.database.azure.com","dawid","lphR9to/q=o#yZjB;'=>","fakebank");
+        $db = mysqli_init();
+        mysqli_ssl_set($db,NULL,NULL, "./BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+        mysqli_real_connect($db, "fakebankdawidordon.mysql.database.azure.com", "dawid", "lphR9to/q=o#yZjB;'=>", "fakebank", 3306, MYSQLI_CLIENT_SSL);
+        //$db = new mysqli("fakebankdawidordon.mysql.database.azure.com","dawid","lphR9to/q=o#yZjB;'=>","fakebank");
         if (mysqli_connect_errno() != 0)
         {
             echo mysqli_error($db);
