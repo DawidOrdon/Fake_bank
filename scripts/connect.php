b4dcb2491ca1ec18b8d9a9133c415f536862e71d
dawid2k20 <dawid.ordon2000@gmail.com>
2023-05-14 15:49:32 +0200

    Update connect.php

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,7 +1,8 @@
 <?php
     function connect()
     {
-        $db = new mysqli("localhost","root","","wsb_fake_bank");
+        //$db = new mysqli("localhost","root","","wsb_fake_bank");
+        $db = new mysqli("sql105.epizy.com","epiz_34205781","yacUtZVAteCdyCWHvYSN","epiz_34205781_fakebank");
         if (mysqli_connect_errno() != 0)
         {
             echo mysqli_error($db);
