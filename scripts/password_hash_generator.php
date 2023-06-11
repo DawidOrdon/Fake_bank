8c41043b0c01acca8eafd028ae88af5b0a30f412
dawid2k20 <dawid.ordon2000@gmail.com>
2023-06-11 13:53:05 +0200

    Update password_hash_generator.php

--- scripts/password_hash_generator.php
+++ scripts/password_hash_generator.php
@@ -2,21 +2,26 @@
     function generate_hash($db,$pass,$id_user)
     {
         $pass= str_split($pass);
-        $sql="select * from patterns where number_of_characters=".count($pass)."";
-        $result=$db->query($sql);
+        $sql=$db->prepare("select * from patterns where number_of_characters=?");
+        $pass_number=count($pass);
+        $sql->bind_param('i',$pass_number);
+        $sql->execute();
+        $result=$sql->get_result();
         while($rekord = $result->fetch_assoc())
         {
-            $sql2=("SELECT pass_char FROM pattern_char WHERE id_pattern=".$rekord["id"]);
-            $pass_char=$db->query($sql2);
+            $sql=$db->prepare("SELECT pass_char FROM pattern_char WHERE id_pattern=?");
+            $sql->bind_param('i',$rekord["id"]);
+            $sql->execute();
+            $pass_char=$sql->get_result();
             $password="";
             while($pass_rekord=$pass_char->fetch_assoc())
             {
                  $password.=$pass[$pass_rekord["pass_char"]-1];
             }
             $pass_hash=password_hash($password,PASSWORD_DEFAULT, ['cost' => 8]);
-            $sql="INSERT INTO `users_hash` (`id`, `id_user`, `id_pattern`, `hash`) VALUES ('', '$id_user', '".$rekord['id']."', '$pass_hash')";
-            $db->query($sql);
-            //echo"$pass_hash<br />";
+            $sql=$db->prepare("INSERT INTO `users_hash` (`id`, `id_user`, `id_pattern`, `hash`) VALUES ('', ?, ?, ?)");
+            $sql->bind_param('iis',$id_user,$rekord['id'],$pass_hash);
+            $sql->execute();
         }
     }
 ?>
